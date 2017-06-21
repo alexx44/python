4e8b06d83e40657068a2c5482223444922e83f07
Juan Muñoz <cl.jmunoz@gmail.com>
2017-06-21 14:32:57 -0400

    test

--- test.py
+++ test.py
@@ -16,13 +16,15 @@ def desorden(lista):
    for character in lista:
       letra = list(lista[i])
       word = list(letra[0])
+      contar = 0
+      indice = 0
       for li in word:
-         print li        
-      
+         if word[indice] <= word[indice + 1]:
+            contar += 1
+         indice += 1
       i = i + 1
-      
-
-   
+     
+  
    
 
 if __name__ == "__main__":
